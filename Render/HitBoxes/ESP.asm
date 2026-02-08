d8d244a7af6d509d1aeaaec78b5041ae950b891f
cocokain <sonehaya53@gmail.com>
2026-02-08 11:44:12 +0900

    Create ESP.asm

---
+++ Render/HitBoxes/ESP.asm
@@ -0,0 +1,22 @@
+0x68AF08 ; Enabled Hitbox
+NOP
+
+0x68AF40 ; Color1
+ORR W2, WZR, #0xFFFFFF ; white
+
+0x68B114 ; YawColor
+ORR W1, WZR, #0xFF ; blue
+
+0x68B0DC ; Eye
+ORR W1, WZR, #0xFF0000
+
+0x68AF384 ; 0 or 1
+MOVZ W0, #1
+
+------------------------------------
+0x68B47C ; ESP
+FCVT S0, D4
+
+0x68B484 ; Border
+FCVT S1, D4
+FCVT S2, D4
