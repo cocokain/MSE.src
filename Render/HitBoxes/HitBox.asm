8376d31ddff9a5302f826e4053b6ef3f2a58077e
cocokain <sonehaya53@gmail.com>
2026-02-08 11:40:53 +0900

    Create HitBox.asm

---
+++ Render/HitBoxes/HitBox.asm
@@ -0,0 +1,14 @@
+0x68AF08 ; Enabled Hitbox
+NOP
+
+0x68AF40 ; Color1
+ORR W2, WZR, #0xFFFFFF ; white
+
+0x68B114 ; YawColor
+ORR W1, WZR, #0xFF ; blue
+
+0x68B0DC ; Eye
+ORR W1, WZR, #0xFF0000
+
+0x68AF384 ; 0 or 1
+MOVZ W0, #1
