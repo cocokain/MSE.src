c0343f20de17d5e762f7e6c6db463ea59e75c789
cocokain <sonehaya53@gmail.com>
2026-02-08 11:22:15 +0900

    Create BiomeSelectMenu.asm

---
+++ System/BiomeSelectMenu.asm
@@ -0,0 +1,9 @@
+0x00000071005ec1dc,U,000208,_ZN21UIScene_SuperFlatMenuC1EiPvP7UILayer
+0x000000710056cd00,U,000872,_ZN24UIScene_BiomePreviewMenuC1EiPvP7UILayer
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+使い方：ワールド生成でスーパーフラットメニューを押すとバイオーム選択メニューが表示されます。
+機能はするが未実装でまだ完成はしていない様子。
+
+0x5EC1DC
+B #0x56CD00 ; SuperFlatMenuへ飛ばす
